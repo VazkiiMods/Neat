f6fa2a85ada68123c5447f373ed8321b976cd485
Vasco Lavos <vazkii@hotmail.com>
2015-11-28 18:01:26 +0000

    Starting off

---
+++ src/main/java/vazkii/neat/Neat.java
@@ -0,0 +1,9 @@
+package vazkii.neat;
+
+import vazkii.neat.lib.LibMisc;
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = LibMisc.MOD_ID, name = LibMisc.MOD_NAME, version = LibMisc.VERSION)
+public class Neat {
+
+}
---
+++ src/main/java/vazkii/neat/lib/LibMisc.java
@@ -0,0 +1,9 @@
+package vazkii.neat.lib;
+
+public final class LibMisc {
+
+	public static final String MOD_ID = "neat";
+	public static final String MOD_NAME = "Neat";
+	public static final String BUILD = "GRADLE:BUILD";
+	public static final String VERSION = "GRADLE:VERSION-" + BUILD;	
+}
