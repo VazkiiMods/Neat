b42e268e14adf8917ec3584d40a3c6f6b8d3c2de
Vaelzan <vaelzan@evilcult.net>
2020-06-11 02:18:51 +1000

    Prevent Neat from triggering "Incompatible FML modded server" message when installed on client only.
    Signed-off-by: Vaelzan <vaelzan@evilcult.net>

--- src/main/java/vazkii/neat/Neat.java
+++ src/main/java/vazkii/neat/Neat.java
@@ -1,9 +1,13 @@
 package vazkii.neat;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod(Neat.MOD_ID)
 public class Neat {
@@ -11,6 +15,7 @@ public class Neat {
 	public static final String MOD_ID = "neat";
 
 	public Neat() {
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         NeatConfig.init();
 	}
