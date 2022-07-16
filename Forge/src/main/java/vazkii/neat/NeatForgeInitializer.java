14ffc4857260a60034b9ee808ab098b168c5fbcc
Vincent Lee <vincent@vincent-lee.net>
2022-07-16 12:55:17 -0700

    Update officially to 1.19 to handle forge's rendering changes

--- Forge/src/main/java/vazkii/neat/NeatForgeInitializer.java
+++ Forge/src/main/java/vazkii/neat/NeatForgeInitializer.java
@@ -1,10 +1,9 @@
 package vazkii.neat;
 
-import net.minecraftforge.client.ClientRegistry;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.network.NetworkConstants;
 
@@ -13,12 +12,11 @@ public class NeatForgeInitializer {
 
 	public NeatForgeInitializer() {
 		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (incoming, isNetwork) -> true));
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::registerKey);
 		NeatForgeConfig.init();
 	}
 
-	public void setup(FMLClientSetupEvent event) {
-		// Constructing KEY itself accesses global vanilla state which is incompatible with parallel mod loading
-		event.enqueueWork(() -> ClientRegistry.registerKeyBinding(ToggleKeybind.KEY));
+	private void registerKey(RegisterKeyMappingsEvent event) {
+		event.register(ToggleKeybind.KEY);
 	}
 }
