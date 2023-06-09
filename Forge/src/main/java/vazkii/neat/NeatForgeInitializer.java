b202c9cbfa0a9dc41500b3aa540342d07973e8bd
Vincent Lee <vincent@vincent-lee.net>
2023-06-08 22:25:08 -0700

    Use EventBusSubscriber on Forge to avoid loading client classes
    Even if the mod is dropped in a server install. Closes #125

--- Forge/src/main/java/vazkii/neat/NeatForgeInitializer.java
+++ Forge/src/main/java/vazkii/neat/NeatForgeInitializer.java
@@ -1,10 +1,11 @@
 package vazkii.neat;
 
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.network.NetworkConstants;
 
 @Mod(NeatConfig.MOD_ID)
@@ -12,11 +13,14 @@ public class NeatForgeInitializer {
 
 	public NeatForgeInitializer() {
 		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (incoming, isNetwork) -> true));
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::registerKey);
 		NeatForgeConfig.init();
 	}
 
-	private void registerKey(RegisterKeyMappingsEvent event) {
-		event.register(ToggleKeybind.KEY);
+	@Mod.EventBusSubscriber(modid = NeatConfig.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+	public static class ClientEventHandler {
+		@SubscribeEvent
+		public static void registerKey(RegisterKeyMappingsEvent event) {
+			event.register(ToggleKeybind.KEY);
+		}
 	}
 }
