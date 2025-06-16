1c5c7eb38df3e5d375aec75dc08f7c4b3b4c085b
Uraneptus <uraneptus33@gmail.com>
2025-06-16 12:47:16 +0200

    start port

--- Xplat/src/main/java/vazkii/neat/HealthBarRenderer.java
+++ Xplat/src/main/java/vazkii/neat/HealthBarRenderer.java
@@ -18,9 +18,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.util.Mth;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
@@ -182,12 +180,15 @@ public class HealthBarRenderer {
 		if (cameraEntity instanceof Player cameraPlayer
 				&& living.isInvisibleTo(cameraPlayer)) {
 			boolean wearingThings = false;
-			for (ItemStack armorSlot : living.getArmorSlots()) {
+			for (EquipmentSlot equipmentslot : EquipmentSlotGroup.ARMOR) {
+				ItemStack armorSlot = living.getItemBySlot(equipmentslot);
 				if (!armorSlot.isEmpty()) {
 					wearingThings = true;
 				}
 			}
-			for (ItemStack handSlot : living.getHandSlots()) {
+
+			for (EquipmentSlot equipmentslot : EquipmentSlotGroup.HAND) {
+				ItemStack handSlot = living.getItemBySlot(equipmentslot);
 				if (!handSlot.isEmpty()) {
 					wearingThings = true;
 				}
--- Xplat/src/main/java/vazkii/neat/NeatRenderType.java
+++ Xplat/src/main/java/vazkii/neat/NeatRenderType.java
@@ -1,27 +1,23 @@
 package vazkii.neat;
 
-import com.mojang.blaze3d.vertex.VertexFormat;
-
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.TriState;
-
-import vazkii.neat.mixin.AccessorRenderType;
-
-import static com.mojang.blaze3d.vertex.DefaultVertexFormat.NEW_ENTITY;
 
 public class NeatRenderType extends RenderStateShard {
 
 	//https://github.com/UpcraftLP/Orderly/blob/master/src/main/resources/assets/orderly/textures/ui/default_health_bar.png
 	public static final ResourceLocation HEALTH_BAR_TEXTURE = ResourceLocation.fromNamespaceAndPath(NeatConfig.MOD_ID, "textures/ui/health_bar_texture.png");
 	public static final RenderType BAR_RENDER_TYPE = getBarRenderType();
-	public static final RenderType BACKGROUND_RENDER_TYPE = getBackgroundRenderType();
+	public static final RenderType BACKGROUND_RENDER_TYPE = getBarRenderType();
 
 	private NeatRenderType(String string, Runnable r, Runnable r1) {
 		super(string, r, r1);
 	}
 
+	/*
 	private static RenderType getBarRenderType() {
 		RenderType.CompositeState renderTypeState = RenderType.CompositeState.builder()
 				.setShaderState(RENDERTYPE_ENTITY_TRANSLUCENT_SHADER)
@@ -44,5 +40,16 @@ public class NeatRenderType extends RenderStateShard {
 				.setOverlayState(OVERLAY)
 				.createCompositeState(false);
 		return AccessorRenderType.neat_create("neat_health_bar_bg", NEW_ENTITY, VertexFormat.Mode.QUADS, 1536, true, false, renderTypeState);
+	}
+
+	 */
+
+	private static RenderType getBarRenderType() { //TODO
+		RenderType.CompositeState renderTypeState = RenderType.CompositeState.builder()
+				.setTextureState(new RenderStateShard.TextureStateShard(NeatRenderType.HEALTH_BAR_TEXTURE, TriState.FALSE, false))
+				.setLightmapState(LIGHTMAP)
+				.setOverlayState(OVERLAY)
+				.createCompositeState(false);
+		return RenderType.create("neat_health_bar", 1536, true, false, RenderPipelines.ENTITY_TRANSLUCENT, renderTypeState);
 	}
 }
