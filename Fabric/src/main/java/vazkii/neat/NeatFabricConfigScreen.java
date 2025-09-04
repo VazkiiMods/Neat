df358872951d3a15df31108bd57855797f20d0cb
Uraneptus <uraneptus33@gmail.com>
2025-09-04 16:05:14 +0200

    start modmenu integration

---
+++ Fabric/src/main/java/vazkii/neat/NeatFabricConfigScreen.java
@@ -0,0 +1,11 @@
+package vazkii.neat;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public class NeatFabricConfigScreen extends Screen {
+
+    protected NeatFabricConfigScreen(Component title) {
+        super(title);
+    }
+}
